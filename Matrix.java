e26d90356e9d6e9b712d1faf81338b59a5937f39
Kylon-Rose <kylonrose12345@gmail.com>
2024-11-13 16:18:49 -0500

    initialized matrix

--- Matrix.java
+++ Matrix.java
@@ -4,4 +4,10 @@ public class Matrix {
 // Adds color to the numbers that don't move
   final String YELLOW = "\033[43m";
   final String RESET = "\033[0m";
+// Initializes Matrix
+  public Matrix(int Dimension) {
+    this.Dimension = Dimension;
+    Matrix = new int[Dimension][Dimension];
+    System.out.println("Dimensions of Matrix" + Dimension + "x" + Dimension + ":");
+  }
 }
