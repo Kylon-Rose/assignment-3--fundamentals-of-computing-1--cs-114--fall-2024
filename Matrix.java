d054f6ecf8224608223eb281a5598c3b9ca4c1b9
Kylon-Rose <kylonrose12345@gmail.com>
2024-11-13 16:18:29 -0500

    added highlight

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,7 @@
 public class Matrix {
   private int[][] Matrix;
   private int Dimension;
+// Adds color to the numbers that don't move
+  final String YELLOW = "\033[43m";
+  final String RESET = "\033[0m";
 }
